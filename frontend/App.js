a86761daa2bf69b4c222d48b1b83c564b8fdc843
Sunghwan-DS <jeonsung02@gmail.com>
2020-09-23 14:45:05 +0900

    S03P23A410 JSH: [UPDATE] reset stack

--- frontend/App.js
+++ frontend/App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Loding from './components/loading';
 import Login from './screens/login';
 import Home from './screens/home';
@@ -28,52 +28,52 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { StackNavigator } from 'react-navigation';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {StackNavigator} from 'react-navigation';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       isLoggedIn: false,
-    }
-  };
+    };
+  }
   async componentDidMount() {
     // you might want to do the I18N setup here
-    const username = await AsyncStorage.getItem('username')
+    const username = await AsyncStorage.getItem('username');
     if (username !== null) {
-      this.setState({ isLoggedIn: true })
+      this.setState({isLoggedIn: true});
     }
   }
   render() {
     return (
       <NavigationContainer style={styles.container}>
         <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen 
-          name="Login" 
-          component={Login}
-          options={{title:"로그인"}}
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{title: '로그인'}}
           />
-          <Stack.Screen 
-          name="Signup" 
-          component={Signup}
-          options={{title:"회원가입"}}
+          <Stack.Screen
+            name="Signup"
+            component={Signup}
+            options={{title: '회원가입'}}
           />
-          <Stack.Screen 
-          name="Home" 
-          component={Home} 
-          options={{title:"하루세끼"}}
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{title: '하루세끼'}}
           />
         </Stack.Navigator>
       </NavigationContainer>
-    )
+    );
   }
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -86,18 +86,18 @@ const styles = StyleSheet.create({
 
 //redux
 const initialState = {
-  counter: 0
-}
+  counter: 0,
+};
 
-const reducer = (state=initialState, action) => {
-  switch(action.type){
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
     case 'INCREASE_COUNTER': {
-      return{counter:state.counter+1}
+      return {counter: state.counter + 1};
     }
     case 'DECREASE_COUNTER':
-      return{counter:state.counter-1}
+      return {counter: state.counter - 1};
   }
-  return state
-}
+  return state;
+};
 
 export default App;
--- frontend/screens/signup.js
+++ frontend/screens/signup.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import {AsyncStorage} from 'react-native';
+import {CommonActions} from '@react-navigation/native';
 
 // const serverUrl = 'http:10.0.2.2:8080/'
 
@@ -36,7 +37,12 @@ class Signup extends Component {
         if (response.key) {
           AsyncStorage.setItem('auth-token', response.key);
           AsyncStorage.setItem('username', this.state.signupData.username);
-          this.props.navigation.push('Home');
+          this.props.navigation.dispatch(
+            CommonActions.reset({
+              index: 1,
+              routes: [{name: 'Home'}],
+            }),
+          );
         } else if (response.username) {
           alert(response.username);
         } else if (response.email) {
