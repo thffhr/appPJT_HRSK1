3ff18a447b7f302c826d6ed01a03128c5269ea5c
psj8532 <psj8532@naver.com>
2020-09-29 16:16:55 +0900

    S03P23A410-36 PSJ: [ADD] take photo
    Former-commit-id: 23f3b11d66d8bb1d5fcdd3b8f8e976e886da6b1e

--- frontend/screens/home.js
+++ frontend/screens/home.js
@@ -34,7 +34,7 @@ class Home extends Component {
   onCamera = () => {
     const options = {
       title: 'Select Avatar',
-      customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+      // customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
       storageOptions: {
         skipBackup: true,
         path: 'images',
@@ -48,8 +48,8 @@ class Home extends Component {
         console.log('User cancelled image picker');
       } else if (response.error) {
         console.log('ImagePicker Error: ', response.error);
-      } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
+      // } else if (response.customButton) {
+      //   console.log('User tapped custom button: ', response.customButton);
       } else {
         const source = { uri: response.uri };
     
