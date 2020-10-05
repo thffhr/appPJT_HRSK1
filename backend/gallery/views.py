f3dfa6520a754d86ec9ea9baebd812341060171c
Sunghwan-DS <jeonsung02@gmail.com>
2020-10-05 18:54:52 +0900

    S03P23A410 JSH: [ADD] using ML model
    Former-commit-id: a183bdbe1f6606285cd82b94003a46360c5cd5e5

--- backend/gallery/views.py
+++ backend/gallery/views.py
@@ -12,25 +12,65 @@ from django.core.files.base import ContentFile
 import base64
 # from django.http import FileResponse
 # Create your views here.
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
 
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def saveMenu(request):
+    def predict_img(uri):
+        def preprocess_input(x):
+            x_copy = np.copy(x)
+            x_copy = x_copy.astype('float32')
+            x_copy = x_copy*1./255
+            return x_copy
+
+        model = load_model('inceptionv3_multiclass_best.h5')
+
+        img_path = uri
+        img = image.load_img(img_path, target_size=(
+            299, 299))  # input 사이즈에 맞게 변환
+
+        x = image.img_to_array(img)
+        x = preprocess_input(x)
+        x = np.expand_dims(x, axis=0)
+
+        r = open('label.txt', mode='rt', encoding='utf-8')
+        lines = r.readlines()
+        results = []
+        for line in lines:
+            results.append(line[12:])
+
+        res = model.predict(x)
+        top_label_ix = np.argmax(res)
+        print('최고점수:', res[0][top_label_ix])
+        print('적중음식:', results[top_label_ix])
+        ord = np.argsort(res)[0][::-1]
+
+        # for i in range(5):
+        #     print(results[ord[i]][:-2])
+        return results[ord[0]][:-2]
+
     new_menu = Menu()
     new_menu.user = request.user
     # type, fileName, data << 각각 프론트에서 보낼 수 있는 데이터
     decoded_data = base64.b64decode(request.data['data'])
     new_menu.image = ContentFile(
         decoded_data, name=f"{request.data['fileName']}")
+    print(type(new_menu.image))
+    # predict_img(new_menu.image)
     new_menu.save()
+    predict = predict_img('media/' + str(new_menu.image))
+    print(predict)
     return Response("파일을 저장했습니다.")
     # response = FileResponse(open(f"media/image/{request.data['fileName']}", 'rb'))
     # return response
 
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
+@ api_view(['POST'])
+@ permission_classes([IsAuthenticated])
 def delImg(request, image_id):
     image = get_object_or_404(Menu, pk=image_id)
     if image.user == request.user or request.user.is_superuser:
@@ -41,8 +81,8 @@ def delImg(request, image_id):
 # 일단 내 사진 목록에서 보여줄 용도로 하나 만들어보겠음
 
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
+@ api_view(['POST'])
+@ permission_classes([IsAuthenticated])
 def myImgs(request):
     images = Menu.objects.order_by('-pk').filter(user=request.user)
     my_imgs = []
@@ -58,8 +98,8 @@ def getImage(request, uri):
     return HttpResponse(images, content_type="image/png")
 
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
+@ api_view(['GET'])
+@ permission_classes([IsAuthenticated])
 def getChart(request):
     Menus = Menu.objects.filter(user=request.user)
     for i in range(len(Menus)):
@@ -69,8 +109,8 @@ def getChart(request):
     return Response('보냈당')
 
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
+@ api_view(['GET'])
+@ permission_classes([IsAuthenticated])
 def getCalendar(request):
     Menus = Menu.objects.filter(user=request.user)
     MenusDict = {}
