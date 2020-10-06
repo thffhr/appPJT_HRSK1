169d53d8fb7354c6bd515334a46a74dd8fd52e67
psj8532 <psj8532@naver.com>
2020-10-06 18:21:55 +0900

    S03P23A410-102 PSJ: [ADD] made js for User Feed
    Former-commit-id: ae61a0275eadee4501f38eeb621e18cb13087fdf

---
+++ frontend/screens/Community/user_feed.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import {View, StyleSheet, ScrollView} from 'react-native';
+
+const serverUrl = 'http://10.0.2.2:8080/';
+
+class UserFeed extends Component {
+  constructor(props) {
+    super(props);
+
+    // this.state
+  }
+  componentDidMount() {}
+  render() {
+    return <View></View>;
+  }
+}
+
+const styles = StyleSheet.create({});
