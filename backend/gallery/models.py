4c5d1661fa3c92d8fd33d36173a84bca4d02a17e
kimsunghyun <tjdgus2319@gmail.com>
2020-10-08 03:50:15 +0900

    class menu2food
    Former-commit-id: aeffefd4dd2529fa80033d095c839c13e2ff6012

--- backend/gallery/models.py
+++ backend/gallery/models.py
@@ -14,3 +14,10 @@ class Menu(models.Model):
         Food, related_name="foods", blank=True)  # 음식 이름, 칼로리, 영양성분(탄/단/지)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     totalCal = models.IntegerField(null=True)
+    
+class Menu2food(models.Model):
+    image = models.ForeignKey(Menu,
+                              on_delete=models.CASCADE)  # Menuid가 들어가는거
+    food = models.ForeignKey(Food,
+                             on_delete=models.CASCADE)  # food id가 들어가는거
+    location = models.TextField(null=False)  # 좌상우하, 좌표값
